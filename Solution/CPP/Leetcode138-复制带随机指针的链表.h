4ad5d9582c1330f7e8d2763a82787ff6ccd288b4
simonhancrew <597494370@qq.com>
2021-07-22 11:14:05 +0800

    daily:客隆图的类似问题

--- Solution/CPP/Leetcode138-复制带随机指针的链表.h
+++ Solution/CPP/Leetcode138-复制带随机指针的链表.h
@@ -14,6 +14,23 @@ public:
 };
 */
 //有random的链表深拷贝
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
@@ -40,5 +57,62 @@ public:
             i++;
         }
         return node_vec[0];
+    }
+};
+
+
+// 先创建全部的节点，再映射一下
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        auto dummy = new Node(-1);
+        auto nh = dummy;
+        auto tmp = head;
+        unordered_map<Node*,Node*> mp;
+        while(tmp){
+            nh ->next = new Node(tmp->val);
+            nh = nh->next;
+            mp[tmp] = nh;
+            tmp = tmp->next;
+        }
+        nh = dummy->next;
+        tmp = head;
+        while(tmp){
+            if(tmp->random){
+                nh->random = mp[tmp->random];
+            }
+            tmp = tmp->next;
+            nh = nh->next;
+        }
+        return dummy->next;
+    }
+};
+
+// 原位构建小弟，无额外空间
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        auto dummy = new Node(-1);
+        // 构建节点的尾后新节点
+        for(auto p = head;p;p = p->next->next){
+            auto q = new Node(p->val);
+            q->next = p->next;
+            p->next = q;
+        }
+        // 构建random边
+        for(auto p = head;p;p = p->next->next){
+            if(p->random){
+                p->next->random = p->random->next;
+            }
+        }
+        // 拆分链表
+        auto cur = dummy;
+        for(auto p = head;p;p = p->next){
+            auto q = p->next;
+            cur->next = q;
+            cur = cur->next;
+            p->next = q->next;
+        }
+        return dummy->next;
     }
 };
--- Solution/sbgo/ADT.go
+++ Solution/sbgo/ADT.go
@@ -12,3 +12,9 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+type Node struct {
+	Val    int
+	Next   *Node
+	Random *Node
+}
