bcb82bac6c82011b25e7271124772f977f61abb5
simonhancrew <597494370@qq.com>
2021-07-28 16:51:52 +0800

    稀疏图的dijkstra

--- XJBX/SCL/Graph/最短路/Dijkstra/稀疏图优化Dijsktra.cpp
+++ XJBX/SCL/Graph/最短路/Dijkstra/稀疏图优化Dijsktra.cpp
@@ -1,63 +1,66 @@
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 #include <queue>
-
-//如果是一个稀疏图，点数超级多的话，用朴素的做法会爆
-//我们需要找一找优化的思路，首先我们会拿到不在最短距离集合中的最近点，我们采用遍历的思路去拿到这个数的
-//但是考虑一个最小堆的话，我们可以O(1)的拿到这个离起点最近的点
 
 using namespace std;
 
-typedef pair<int,int> PII;
+const int N = 150000;
 
-const int N = 150010;
-
-int h[N],e[N],w[N],ne[N],idx;
+int h[N],e[N],ne[N],w[N],idx;
 int d[N];
 bool st[N];
+
 int n,m;
 
-void add(int a,int b,int c){
+typedef pair<int, int> PII;
+
+void add(int a,int b,int c)
+{
+    // w[i]，当前b到a的距离
     e[idx] = b,ne[idx] = h[a],w[idx] = c,h[a] = idx++;
 }
 
-
-int dijsktra(){
+// 堆优化dijkstra
+int dijkstra(){
+    // 1 初始距离
     memset(d,0x3f,sizeof d);
     d[1] = 0;
-    
+    // 2 利用最小堆来找不在确定集合中的最小距离
     priority_queue<PII,vector<PII>,greater<PII>> heap;
     heap.push({0,1});
-    
     while(heap.size()){
         auto t = heap.top();
         heap.pop();
-        int index = t.second,distance = t.first;
-        
-        if(st[index]) continue;
-        st[index] = true;
-        
-        for(int i = h[index];i != -1;i = ne[i]){
+        int node = t.second,dis = t.first;
+        // 2 按照第一个元素排序，可能存在冗余，直接判断就好
+        if(st[node]) continue;
+        // 3 将这个最小的距离加入确定集合
+        st[node] = true;
+        // 4 利用这个距离来更新他周围的点到起点的距离
+        for(int i = h[node];i != -1;i = ne[i]){
             int j = e[i];
-            if(d[j] > distance + w[i]){
-                d[j] = distance + w[i]; 
+            if(d[j] > d[node] + w[i]){
+                d[j] = d[node] + w[i];
                 heap.push({d[j],j});
-            }    
+            }
         }
     }
+    // 5 判断
     if(d[n] == 0x3f3f3f3f) return -1;
     return d[n];
 }
 
-
-int main(){
+int main()
+{
     cin >> n >> m;
     memset(h,-1,sizeof h);
+    // 邻接表的带权边
     while(m--){
-        int a,b,c;
-        scanf("%d%d%d",&a,&b,&c);
-        add(a,b,c);
+        int x,y,z;
+        cin >> x >> y >> z;
+        add(x,y,z);
     }
-    cout << dijsktra() << endl;
+    cout << dijkstra() << endl;
     return 0;
 }
